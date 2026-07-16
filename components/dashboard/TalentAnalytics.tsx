6bc6d95268ab2483cf4335979b6b7170d0247b64
ttalgi nongbu <aryadimas.works@gmail.com>
2026-07-16 21:40:57 +0700

    refactor(dashboard): pisahkan analytics ke dalam komponen

---
+++ components/dashboard/TalentAnalytics.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function TalentAnalytics() {
+  return (
+    <div
+      className="
+        mt-8
+        rounded-2xl
+        border
+        border-gray-200
+        bg-white
+        p-6
+        shadow-sm
+      "
+    >
+      Hello World
+    </div>
+  );
+}

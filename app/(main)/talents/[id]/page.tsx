6bd7dad8ff0a1b3b6ac6274db8b7af3dd7378e9f
ttalgi nongbu <aryadimas.works@gmail.com>
2026-07-18 14:24:27 +0700

    feat(talent-management): tambah halaman detail talent
    - tambahkan route detail talent menggunakan dynamic route /talents/[id]
    - tambahkan struktur komponen detail dengan folder terpisah
    - hubungkan aksi View pada daftar talent ke halaman detail
    - siapkan struktur awal halaman detail untuk pengembangan selanjutnya

---
+++ app/(main)/talents/[id]/page.tsx
@@ -0,0 +1,23 @@
+import Content from "@/components/talent-management/detail/Content";
+
+export default function TalentDetailPage() {
+  return (
+    <div
+      className="
+        bg-gray-100
+        px-0
+        sm:p-6
+      "
+    >
+      <div
+        className="
+          mx-auto
+          w-full
+          max-w-[1150px]
+        "
+      >
+        <Content />
+      </div>
+    </div>
+  );
+}
---
+++ components/talent-management/detail/Content.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+export default function Content() {
+  return (
+    <section
+      className="
+        rounded-2xl
+        bg-white
+        p-8
+      "
+    >
+      <h1
+        className="
+          text-2xl
+          font-bold
+          text-gray-800
+        "
+      >
+        Talent Detail
+      </h1>
+
+      <p
+        className="
+          mt-2
+          text-gray-500
+        "
+      >
+        Hello World
+      </p>
+    </section>
+  );
+}

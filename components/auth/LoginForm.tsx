4a41b391ed97ac734b1b75989868f660bcfe2834
ttalgi nongbu <aryadimas.works@gmail.com>
2026-07-26 17:50:37 +0700

    feat(loginform): update lagi tampilan login

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -18,18 +18,15 @@ export default function LoginForm() {
   return (
     <div
       className="
-  min-h-screen
-  flex
-  justify-center
-  bg-white
-  px-4
-  pt-12
-  pb-8
-  text-gray-900
-
-  sm:px-6
-  sm:pt-20
-"
+      min-h-screen
+      flex
+      items-center
+      justify-center
+      bg-white
+      text-gray-900
+      px-4
+      sm:px-6
+    "
     >
       <div
         className="
